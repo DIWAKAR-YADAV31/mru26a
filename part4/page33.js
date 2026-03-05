2a91e0b871396c2693623efb319814e7b8ff0e95
DARGA DIWAKAR <dargadiwakar@gmail.com>
2026-03-05 14:55:07 +0530

    updated

--- part4/page33.js
+++ part4/page33.js
@@ -1,12 +1,13 @@
-function add(a,b){
-    return a+b;
-}
-
-function sub(a,b){
-    return a+b;
-}
+import {add,sub} from "./calc.js"
 
 const result1=add(5,4)
-console.log(result)
+console.log(result1)
 const result2=sub(5,4)
 console.log(result2)
+
+
+
+// const result1=add(5,4)
+// console.log(result)
+// const result2=sub(5,4)
+// console.log(result2)
