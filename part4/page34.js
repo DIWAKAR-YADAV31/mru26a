87b43148192d0ba3fd11bc5f4c3016cb3047ba68
DARGA DIWAKAR <dargadiwakar@gmail.com>
2026-03-06 12:00:56 +0530

    updated

--- part4/page34.js
+++ part4/page34.js
@@ -4,12 +4,40 @@ const app=express();
 app.set("view engine","ejs")
 app.set("views","views")
 
-app.listen(8080,()=>console.log("server Started"));
+app.listen(5000,()=>console.log("server Started"));
+app.use(express.urlencoded({extended:true}));
+
+
+//arrey called users
+const users=[
+    {name:"diwakar",email:"diwakardarga@gmail.com",password:"1234"},
+    {name:"DD",email:"dargadiwakar@gmail.com",password:"abcd"},
+    {name:"Darga",email:"darga@gmail.com",password:"king"}
+];
 
 app.get("/login",(req,res)=>{
     res.render("login")
 });
 
+app.post("/login",(req,res)=>{
+    const {email,password}=req.body
+    const user=users.find((user)=>user.email===email);
+    if(user){
+        if(user.password===password){
+            res.redirect("/");
+        }else{
+            res.redirect("/login");
+        }
+    }else{
+        res.redirect("/login")
+    }
+    res.redirect("/")
+})
+
+
 app.get("/register",(req,res)=>{
     res.render("register")
+})
+app.get("/",(req,res)=>{
+    res.render("dashboard",{users})
 })
